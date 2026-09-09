a253d57176841184d06b7cede5e90a40cb34fe30
Axel <axelddlima@hotmail.com>
2026-09-09 14:53:56 -0300

    implementando atividade final.
    Signed-off-by: Axel <axelddlima@hotmail.com>

--- src/Grafo.java
+++ src/Grafo.java
@@ -1,19 +1,50 @@
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Grafo {
-    static void main(String[] args) {
-        //estou com uma plantação de abacaxi, quero encontrar compradores de abacaxi! Vamos procurar na minha rede de amigos...
-        Map<String, List<String>> redeDeAmigos = new HashMap<>();
-        //Grau 1
-        redeDeAmigos.put("Axel", List.of("João", "Maria"));
-        //Grau 2
-        redeDeAmigos.put("João", List.of("Mick", "Marolu"));
-        //Grau 2
-        redeDeAmigos.put("Maria", List.of("Leticia", "Lindin"));
-        //Grau 3
-        redeDeAmigos.put("Mick", List.of("Faning", "Pitoco"));
+    //Estou com uma plantação de abacaxi, quero encontrar compradores de abacaxi! Vamos procurar na minha rede de amigos...
+    static Map<String, List<String>> redeDeAmigos;
 
+    static void main(String[] args) {
+        Grafo grafo = new Grafo();
+        /**
+         * Curiosidades:
+         * Vendedores de Abacaxi tem 1 ao final do nome.
+         */
+        //QUEM SOU EU?
+        String eu = "Axel";
+        //MAPEAR REDE DE AMIGOS
+        grafo.popularRedeDeAmigos();
+        //PEGAR A MINHA REDE DE AMIGOS GRAU 1
+        Queue<String> fila = new LinkedList<>(redeDeAmigos.get(eu));
+        //O COMPRADOR AINDA NÃO FOI ENCONTRADO - :/ :\ :| :(
+        var compradorDeAbacaxiEncontrado = false;
+
+        while(!compradorDeAbacaxiEncontrado) {
+            //PESQUISA EM LARGA ESCALA, PROCURAR NO MAIS PRÓXIMO
+            var amigo = fila.poll();
+            compradorDeAbacaxiEncontrado = amigo.contains("1");
+            //SE ENCONTRAR O COMPRADOR, MOSTRAR QUEM É, E SAI.
+            if (compradorDeAbacaxiEncontrado){
+                System.out.println(amigo);
+                break;
+            } else if (redeDeAmigos.get(amigo)!=null && !redeDeAmigos.get(amigo).isEmpty()) { // SE AMIGO TIVER REDE, PEGAR REDE DO AMIGO.
+                fila.addAll(redeDeAmigos.get(amigo));
+            } else if (fila.isEmpty()) {
+                System.out.println("Não existem comprador de abacaxi na rede de amigos!");
+                break;
+            }
+        }
+    }
+
+    private void popularRedeDeAmigos() {
+        redeDeAmigos = new HashMap<>();
+        //Grau 1
+        redeDeAmigos.put("Axel", List.of("Philipe", "Iury"));
+        //Grau 2
+        redeDeAmigos.put("Philipe", List.of("Mick", "Marolu"));
+        //Grau 2
+        redeDeAmigos.put("Iury", List.of("Leticia", "Lindin"));
+        //Grau 3
+        redeDeAmigos.put("Mick", List.of("Faning1", "Pitoco"));
     }
 }
